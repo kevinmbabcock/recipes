dbee1de4af9b94eb4693cf2a4bb8986db43c13fa
Guest User <Guest@epicodus-20.local>
2017-12-13 16:34:36 -0800

    add views for ingredients and tags

--- app.rb
+++ app.rb
@@ -27,3 +27,30 @@ get("/tags") do
   @tags = Tag.all
   erb(:tags)
 end
+
+post("/tags") do
+  name = params[:tag_name]
+  Tag.create({:name => name})
+  redirect('/tags')
+end
+
+get("/tags/:id") do
+  @tag = Tag.find(params[:id].to_i)
+  erb(:tag)
+end
+
+get("/ingredients") do
+  @ingredients = Ingredient.all
+  erb(:ingredients)
+end
+
+post("/ingredients") do
+  name = params[:ingredient_name]
+  Ingredient.create({:name => name})
+  redirect('/ingredients')
+end
+
+get("/ingredients/:id") do
+  @ingredient = Ingredient.find(params[:id].to_i)
+  erb(:ingredient)
+end
--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,7 +1,7 @@
 require('spec_helper')
 
 describe(Ingredient) do
-  it("belongs to a recipe") do
+  it("has many recipes") do
     test_recipe = Recipe.create({:name => "cheesecake"})
     test_recipe2 = Recipe.create({:name => "cookies"})
     new_ingredient1 = Ingredient.create({:name => "flour", :recipe_ids => [test_recipe.id, test_recipe2.id]})
