34e4327ae8fb0d73046d511f2b474d4f46028fe9
Guest User <Guest@epicodus-20.local>
2017-12-13 16:04:46 -0800

    add multiple ingredients to a recipe

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -7,7 +7,10 @@ describe(Recipe) do
     test_recipe = Recipe.create({:name => "cheesecake", :tag_ids => [test_tag.id, test_tag1.id]})
     expect(test_recipe.tags).to(eq([test_tag, test_tag1]))
   end
-  # it("has many ingredients") do
-  #
-  # end
+  it("has many ingredients") do
+    test_ingredient1 = Ingredient.create({:name => "flour"})
+    test_ingredient2 = Ingredient.create({:name => "sugar"})
+    test_recipe = Recipe.create({:name => "cheesecake", :ingredient_ids => [test_ingredient1.id, test_ingredient2.id]})
+    expect(test_recipe.ingredients).to(eq([test_ingredient1, test_ingredient2]))
+  end
 end
